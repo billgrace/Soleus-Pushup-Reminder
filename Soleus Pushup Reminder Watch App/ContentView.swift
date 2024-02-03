d63ac9bf1a8d047357695afa5c71a219e91e9059
billgrace <bill@billgrace.com>
2024-02-02 16:18:05 -0800

    Initial Commit

---
+++ Soleus Pushup Reminder Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Soleus Pushup Reminder Watch App
+//
+//  Created by Bill Grace on 2/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Soleus Pushup Reminder Watch App/Soleus_Pushup_ReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Soleus_Pushup_ReminderApp.swift
+//  Soleus Pushup Reminder Watch App
+//
+//  Created by Bill Grace on 2/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Soleus_Pushup_Reminder_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
